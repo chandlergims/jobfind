4fa353a2f89cd2ae59f0862210fecb88f7fbb504
chandlergims <chandlergims@gmail.com>
2025-03-12 17:13:18 -0400

    Update Twitter link to X.com/TrenchLink

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -266,7 +266,7 @@ const Sidebar = () => {
                 <a 
                   target="_blank" 
                   className="peer flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left outline-none transition-all hover:bg-gray-100 h-8 text-sm" 
-                  href="https://twitter.com/trenchedin"
+                  href="https://x.com/TrenchLink"
                 >
                   <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                     <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"></path>
